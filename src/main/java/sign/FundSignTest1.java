896aba60823f8d37237285d3ff5c7234d9237aca
zoudongxia <809569765@qq.com>
2018-11-29 17:18:44 +0800

    'signtest'

--- src/main/java/sign/FundSignTest1.java
+++ src/main/java/sign/FundSignTest1.java
@@ -11,7 +11,7 @@ import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.LinkedHashMap;
 
-public class FindSignTest {
+public class FundSignTest1 {
     public static String signWithMD5(String jsonString,String secret){
 
         try {
